9f81ba905f90dc5f4696e7ceab650b7683e99b31
northinrtm <artsdurden@gmail.com>
2022-08-03 15:36:56 +0300

    2 задача

--- src/Main.java
+++ src/Main.java
@@ -6,5 +6,11 @@ public class Main {
         long l = 4444L;
         float f = 5.5555f;
         double d = 6.66666d;
+
+        float weightFighter1 = 78.2f;
+        float weightFighter2 = 82.7f;
+        System.out.println("Вес бойцов: " + weightFighter1 + weightFighter2);
+        float weightDistinction = weightFighter1 - weightFighter2;
+        System.out.println("Разница в весе между бойцами: " + Math.abs(weightDistinction));
     }
 }
