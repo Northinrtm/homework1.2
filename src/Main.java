a8ee41f91688373ef996e828f3ed292c478465b6
northinrtm <artsdurden@gmail.com>
2022-08-03 15:38:55 +0300

    3 задача

--- src/Main.java
+++ src/Main.java
@@ -6,11 +6,24 @@ public class Main {
         long l = 4444L;
         float f = 5.5555f;
         double d = 6.66666d;
-
+        System.out.println("2 задача:");
         float weightFighter1 = 78.2f;
         float weightFighter2 = 82.7f;
         System.out.println("Вес бойцов: " + weightFighter1 + weightFighter2);
         float weightDistinction = weightFighter1 - weightFighter2;
         System.out.println("Разница в весе между бойцами: " + Math.abs(weightDistinction));
+        System.out.println("3 задача:");
+        int banana = 5;
+        int weightBanana = 80;
+        int Milk = 200;
+        double weightMilk = Milk * (105.0/ 100.0);
+        int iceCream = 2;
+        int weightIceCream = 100;
+        int eggs = 4;
+        int weightEgg = 70;
+        double weightBreakfast = banana * weightBanana + weightMilk + iceCream * weightIceCream +
+                eggs * weightEgg;
+        System.out.println("Общий вес завтрака: " + weightBreakfast);
+        System.out.println("В килограммах это: " + (weightBreakfast / 1000));
     }
 }
