e2680a333812e11cf442c99068d1bca3b0956649
northinrtm <artsdurden@gmail.com>
2022-08-03 15:34:34 +0300

    1 задача

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-
+        byte b = 1;
+        short sh = 22;
+        int i = 333;
+        long l = 4444L;
+        float f = 5.5555f;
+        double d = 6.66666d;
     }
 }
